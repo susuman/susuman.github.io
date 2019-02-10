db3d8d2e626e5ef3b6779852897ef19b67d60717
susuman <44407635+susuman@users.noreply.github.com>
2019-02-10 17:01:00 +0200

    Update client1.js

--- client1.js
+++ client1.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic: 2019.01.08.5";
+document.getElementById("id_logic_version").innerHTML = "Logic: 2019.01.08.0";
 
 var svg = document.getElementById("id_svg");
 
@@ -38,5 +38,5 @@ function on_touch(e)
 {	
 	setTimeout(function(){svg.remove(svg.appendChild);}, 5000)
 	setTimeout(function(){alert("ai apasat de "+nr+" ori");}, 5500);
-	window.stop();
 }
+window.stop();
