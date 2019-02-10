cd4fe263ee9a230e485ff39566125adffee1edb5
susuman <44407635+susuman@users.noreply.github.com>
2019-02-10 14:07:22 +0200

    Add files via upload

--- client1.js
+++ client1.js
@@ -4,6 +4,7 @@ var svg = document.getElementById("id_svg");
 
 svg.addEventListener("touchstart", on_touch_svg);
 svg.addEventListener("mousedown", on_touch_svg);
+svg.addEventListener("touchend", on_touch_svg);
 
 var svg_rect = svg.getBoundingClientRect();
 
@@ -11,11 +12,11 @@ function on_touch_svg(e)
 {
 	for (var i = 0; i < e.changedTouches.length; i++)
 	{
-		var cerc = document.createElementNS("http://www.w3.org/2000/svg", "circle"); //creare element cerc in cazul nostru
-		cerc.setAttribute("cx", e.changedTouches[i].pageX);  //atributele cercului cu valorile din paranteze
+		var cerc = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+		cerc.setAttribute("cx", e.changedTouches[i].pageX);
 		cerc.setAttribute("cy", e.changedTouches[i].pageY - svg_rect.top);
-		cerc.setAttribute("r", 20);
-		svg.appendChild(cerc);  // adaugare in svg ca si "copil" (subelement)
+		cerc.setAttribute("r", 40);
+		svg.appendChild(cerc);
 
 }
 }
