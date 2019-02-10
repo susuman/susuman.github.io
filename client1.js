103b7b1aec67ba8f1a7765ddb81dc37f336a8180
susuman <44407635+susuman@users.noreply.github.com>
2019-02-10 15:01:42 +0200

    Add files via upload

--- client1.js
+++ client1.js
@@ -1,10 +1,10 @@
-document.getElementById("id_logic_version").innerHTML = "Logic: 2019.01.08.2";
+document.getElementById("id_logic_version").innerHTML = "Logic: 2019.01.08.3";
 
 var svg = document.getElementById("id_svg");
 
 svg.addEventListener("touchstart", on_touch_svg);
 //svg.addEventListener("mousedown", on_touch_svg);
-//svg.addEventListener("touchend", on_touch);
+svg.addEventListener("touchend", on_touch);
 //svg.addEventListener("touchmove", on_touch_svg);
 
 var svg_rect = svg.getBoundingClientRect();
@@ -20,6 +20,11 @@ function on_touch_svg(e)
 		cerc.setAttribute("r", 40);
 		cerc.setAttribute("fill", "blue");
 		svg.appendChild(cerc);
-		svg.remove(svg.appendChild);
+		
 }
+}
+
+function on_touch(e)
+{
+	svg.remove(svg.appendChild);
 }
